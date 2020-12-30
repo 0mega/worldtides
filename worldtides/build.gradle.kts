c67bdfc3e39cba8a99e10aae00f9a68e0ce258c4
Oleksandr Kruk <oleksandr.kruk@fundingcircle.com>
2020-12-30 22:39:40 +0000

    Bump Kotlin to 1.4.10

--- worldtides/build.gradle.kts
+++ worldtides/build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
