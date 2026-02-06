a1fcd28318b2aac2df8c4378902798afaeba7eef
Oleksandr Kruk <kruk.oleksandr@gmail.com>
2026-02-06 00:51:01 +0000

    feat: Allow custom OkHttpClient and logging level configuration

--- worldtides/src/main/kotlin/com/oleksandrkruk/worldtides/RetrofitClient.kt
+++ worldtides/src/main/kotlin/com/oleksandrkruk/worldtides/RetrofitClient.kt
@@ -1,4 +1,5 @@
 package com.oleksandrkruk.worldtides
+
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
@@ -6,18 +7,28 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-internal class RetrofitClient(baseUrl: String) {
+internal class RetrofitClient(
+        baseUrl: String,
+        private val customClient: OkHttpClient? = null,
+        private val loggingLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.NONE
+) {
     private val moshiFactory = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
+    val client: OkHttpClient by lazy {
+        customClient
+                ?: run {
+                    val logging = HttpLoggingInterceptor()
+                    logging.level = loggingLevel
+                    OkHttpClient.Builder().addInterceptor(logging).build()
+                }
+    }
+
     val tidesService: WorldTidesGateway by lazy {
-        val logging = HttpLoggingInterceptor()
-        logging.level = HttpLoggingInterceptor.Level.NONE
-        val client = OkHttpClient.Builder().addInterceptor(logging).build()
         Retrofit.Builder()
-            .client(client)
-            .baseUrl(baseUrl)
-            .addConverterFactory(MoshiConverterFactory.create(moshiFactory))
-            .build()
-            .create(WorldTidesGateway::class.java)
+                .client(client)
+                .baseUrl(baseUrl)
+                .addConverterFactory(MoshiConverterFactory.create(moshiFactory))
+                .build()
+                .create(WorldTidesGateway::class.java)
     }
 }
--- worldtides/src/main/kotlin/com/oleksandrkruk/worldtides/WorldTides.kt
+++ worldtides/src/main/kotlin/com/oleksandrkruk/worldtides/WorldTides.kt
@@ -6,67 +6,101 @@ import com.oleksandrkruk.worldtides.models.TideDataType
 import com.oleksandrkruk.worldtides.models.Tides
 import java.text.SimpleDateFormat
 import java.util.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
-class WorldTides private constructor(
+class WorldTides
+private constructor(
         private val apiKey: String,
         private val tidesRepository: WorldTidesRepository,
         private val inputDateFormat: SimpleDateFormat
-    ) {
+) {
 
     class Builder {
         private val apiDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mmZ", Locale.US)
         private val inputDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.US)
-        private val tidesGateway by lazy { RetrofitClient("https://www.worldtides.info/api/").tidesService }
-        private val tidesRepository: WorldTidesRepository by lazy { WorldTidesRepository(tidesGateway, apiDateFormat) }
+        private var okHttpClient: OkHttpClient? = null
+        private var loggingLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.NONE
+
+        private val tidesGateway by lazy {
+            RetrofitClient("https://www.worldtides.info/api/", okHttpClient, loggingLevel)
+                    .tidesService
+        }
+        private val tidesRepository: WorldTidesRepository by lazy {
+            WorldTidesRepository(tidesGateway, apiDateFormat)
+        }
+
+        fun client(client: OkHttpClient): Builder {
+            this.okHttpClient = client
+            return this
+        }
+
+        fun loggingLevel(level: HttpLoggingInterceptor.Level): Builder {
+            this.loggingLevel = level
+            return this
+        }
 
         /**
-         * Create an instance of [WorldTides] using a given [apiKey].
-         * The [apiKey] will be used for all the calls to the worldtides.info API.
+         * Create an instance of [WorldTides] using a given [apiKey]. The [apiKey] will be used for
+         * all the calls to the worldtides.info API.
          *
-         * Note: You should avoid creating many instances of [WorldTides] as it is a costly operation. Your app
-         * should cache an instance of [WorldTides] and reuse it for all the requests you make during
-         * a user session.
+         * Note: You should avoid creating many instances of [WorldTides] as it is a costly
+         * operation. Your app should cache an instance of [WorldTides] and reuse it for all the
+         * requests you make during a user session.
          */
-        fun build(
-            apiKey: String
-        ) : WorldTides {
+        fun build(apiKey: String): WorldTides {
             return WorldTides(apiKey, tidesRepository, inputDateFormat)
         }
     }
 
     /**
-     * Returns only the extremes of the tides (Highs and Lows) between [date] and the number of [days] in future.
+     * Returns only the extremes of the tides (Highs and Lows) between [date] and the number of
+     * [days] in future.
      *
-     * @param [date] should be the starting date from which the tide extremes prediction are requested.
-     * @param [days] should be the number of days for which tide extremes are requested. This number will add
-     * to the [date] and will form a time range. E.g.: If the given [date] is 2020-12-01 and [days]
-     * is 3 it means that the user will receive all tides between 2020-12-01 and 2020-12-03.
+     * @param [date] should be the starting date from which the tide extremes prediction are
+     * requested.
+     * @param [days] should be the number of days for which tide extremes are requested. This number
+     * will add to the [date] and will form a time range. E.g.: If the given [date] is 2020-12-01
+     * and [days] is 3 it means that the user will receive all tides between 2020-12-01 and
+     * 2020-12-03.
      * @param [lat] is the latitude of the location for which the tides are requested.
      * @param [lon] is the longitude of the location for which the tides are requested.
-     * @param [callback] is the callback that will return the results. The [callback] will offer tide extremes
-     * wrapped in a [Result]. When the operation is successful, the result will contain the tide extremes
-     * otherwise it will provide an error.
-     *
+     * @param [callback] is the callback that will return the results. The [callback] will offer
+     * tide extremes wrapped in a [Result]. When the operation is successful, the result will
+     * contain the tide extremes otherwise it will provide an error.
      */
-    fun getTideExtremes(date: Date, days: Int, lat: String, lon: String, callback: (Result<TideExtremes>) -> Unit) {
+    fun getTideExtremes(
+            date: Date,
+            days: Int,
+            lat: String,
+            lon: String,
+            callback: (Result<TideExtremes>) -> Unit
+    ) {
         val dateStr = inputDateFormat.format(date)
         tidesRepository.extremes(dateStr, days, lat, lon, apiKey, callback)
     }
 
     /**
-     * Returns only the extremes of the tides (Highs and Lows) between [date] and the number of [days] in future.
-     * **This method exists for Java interoperability only.**
+     * Returns only the extremes of the tides (Highs and Lows) between [date] and the number of
+     * [days] in future. **This method exists for Java interoperability only.**
      *
-     * @param [date] should be the starting date from which the tide extremes prediction are requested.
-     * @param [days] should be the number of days for which tide extremes are requested. This number will add
-     * to the [date] and will form a time range. E.g.: If the given [date] is 2020-12-01 and [days]
-     * is 3 it means that the user will receive all tides between 2020-12-01 and 2020-12-03.
+     * @param [date] should be the starting date from which the tide extremes prediction are
+     * requested.
+     * @param [days] should be the number of days for which tide extremes are requested. This number
+     * will add to the [date] and will form a time range. E.g.: If the given [date] is 2020-12-01
+     * and [days] is 3 it means that the user will receive all tides between 2020-12-01 and
+     * 2020-12-03.
      * @param [lat] is the latitude of the location for which the tides are requested.
      * @param [lon] is the longitude of the location for which the tides are requested.
      * @param [callback] is the callback that will return either tide or an error.
-     *
      */
-    fun getTideExtremes(date: Date, days: Int, lat: String, lon: String, callback: TidesCallback<TideExtremes>) {
+    fun getTideExtremes(
+            date: Date,
+            days: Int,
+            lat: String,
+            lon: String,
+            callback: TidesCallback<TideExtremes>
+    ) {
         val dateStr = inputDateFormat.format(date)
         tidesRepository.extremes(dateStr, days, lat, lon, apiKey) { result ->
             result.onFailure { callback.error(Error(it)) }
@@ -77,28 +111,42 @@ class WorldTides private constructor(
     /**
      * Returns the predicted tide heights between [date] and the number of [days] in future.
      *
-     * @param [date] should be the starting date from which the tide heights prediction are requested.
+     * @param [date] should be the starting date from which the tide heights prediction are
+     * requested.
      * @param [days] should be the number of days for which tide heights are requested.
      * @param [lat] is the latitude of the location for which the tides are requested.
      * @param [lon] is the longitude of the location for which the tides are requested.
      * @param [callback] is the callback that will return the results wrapped in a [Result].
      */
-    fun getTideHeights(date: Date, days: Int, lat: String, lon: String, callback: (Result<TideHeights>) -> Unit) {
+    fun getTideHeights(
+            date: Date,
+            days: Int,
+            lat: String,
+            lon: String,
+            callback: (Result<TideHeights>) -> Unit
+    ) {
         val dateStr = inputDateFormat.format(date)
         tidesRepository.heights(dateStr, days, lat, lon, apiKey, callback)
     }
 
     /**
-     * Returns the predicted tide heights between [date] and the number of [days] in future.
-     * **This method exists for Java interoperability only.**
+     * Returns the predicted tide heights between [date] and the number of [days] in future. **This
+     * method exists for Java interoperability only.**
      *
-     * @param [date] should be the starting date from which the tide heights prediction are requested.
+     * @param [date] should be the starting date from which the tide heights prediction are
+     * requested.
      * @param [days] should be the number of days for which tide heights are requested.
      * @param [lat] is the latitude of the location for which the tides are requested.
      * @param [lon] is the longitude of the location for which the tides are requested.
      * @param [callback] is the callback that will return either tide heights or an error.
      */
-    fun getTideHeights(date: Date, days: Int, lat: String, lon: String, callback: TidesCallback<TideHeights>) {
+    fun getTideHeights(
+            date: Date,
+            days: Int,
+            lat: String,
+            lon: String,
+            callback: TidesCallback<TideHeights>
+    ) {
         val dateStr = inputDateFormat.format(date)
         tidesRepository.heights(dateStr, days, lat, lon, apiKey) { result ->
             result.onFailure { callback.error(Error(it)) }
@@ -107,8 +155,8 @@ class WorldTides private constructor(
     }
 
     /**
-     * Returns flexible tide data based on the specified [dataTypes].
-     * Supports stacking multiple data types in a single API call.
+     * Returns flexible tide data based on the specified [dataTypes]. Supports stacking multiple
+     * data types in a single API call.
      *
      * @param [date] should be the starting date from which the tide data is requested.
      * @param [days] should be the number of days for which tide data is requested.
@@ -117,14 +165,21 @@ class WorldTides private constructor(
      * @param [dataTypes] list of data types to request (e.g., HEIGHTS, EXTREMES).
      * @param [callback] is the callback that will return the results wrapped in a [Result].
      */
-    fun getTides(date: Date, days: Int, lat: String, lon: String, dataTypes: List<TideDataType>, callback: (Result<Tides>) -> Unit) {
+    fun getTides(
+            date: Date,
+            days: Int,
+            lat: String,
+            lon: String,
+            dataTypes: List<TideDataType>,
+            callback: (Result<Tides>) -> Unit
+    ) {
         val dateStr = inputDateFormat.format(date)
         tidesRepository.tides(dataTypes, dateStr, days, lat, lon, apiKey, callback)
     }
 
     /**
-     * Returns flexible tide data based on the specified [dataTypes].
-     * **This method exists for Java interoperability only.**
+     * Returns flexible tide data based on the specified [dataTypes]. **This method exists for Java
+     * interoperability only.**
      *
      * @param [date] should be the starting date from which the tide data is requested.
      * @param [days] should be the number of days for which tide data is requested.
@@ -133,7 +188,14 @@ class WorldTides private constructor(
      * @param [dataTypes] list of data types to request.
      * @param [callback] is the callback that will return either tide data or an error.
      */
-    fun getTides(date: Date, days: Int, lat: String, lon: String, dataTypes: List<TideDataType>, callback: TidesCallback<Tides>) {
+    fun getTides(
+            date: Date,
+            days: Int,
+            lat: String,
+            lon: String,
+            dataTypes: List<TideDataType>,
+            callback: TidesCallback<Tides>
+    ) {
         val dateStr = inputDateFormat.format(date)
         tidesRepository.tides(dataTypes, dateStr, days, lat, lon, apiKey) { result ->
             result.onFailure { callback.error(Error(it)) }
@@ -141,4 +203,3 @@ class WorldTides private constructor(
         }
     }
 }
-
